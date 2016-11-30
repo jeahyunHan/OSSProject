14e0aff77da2b3662e834d8c2785c425691f544a
DESKTOP-MQCG4UB\cu <macknick@naver.com>
2016-12-01 03:47:07 +0900

    NoteListViewAdapter.java 겟뷰 부분 작업중~~!

--- Note_jonghun/src/com/example/note/NoteListViewAdapter.java
+++ Note_jonghun/src/com/example/note/NoteListViewAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class NoteListViewAdapter extends BaseAdapter{ // 노트 리스트뷰 어댑터  (( 실시간으로 검색결과를 뿌려주기위함)
 
@@ -19,34 +20,47 @@ public class NoteListViewAdapter extends BaseAdapter{ // 노트 리스트뷰 어댑터  (
 	public NoteListViewAdapter(Context context,List<Note> noteList) {
 	
 		this.context = context;
-		
 		this.noteList = noteList;
 		inflater=LayoutInflater.from(context);
 		this.arrayList = new ArrayList<Note>();
 		this.arrayList.addAll(noteList);
 	}
-
+	public class ViewHolder{
+		TextView no_title;
+	}
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return noteList.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return null;
+		return noteList.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(int position, View view, ViewGroup parent) {
 		// TODO Auto-generated method stub
+		final ViewHolder holder;
+		final Note note=noteList.get(position);
+		
+		if(view==null){
+			holder = new ViewHolder();
+			view = inflater.inflate(R.layout.notes_row,null);
+			holder.no_title=(TextView)view.findViewById(R.id.no_title);
+			view.setTag(holder);
+		}else{
+			holder=(ViewHolder)view.getTag();
+		}
+		
 		return null;
 	}
 	
