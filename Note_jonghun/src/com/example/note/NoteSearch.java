adb109bb0d7ca25b281ee4449bc5d6b24836b869
DESKTOP-MQCG4UB\cu <macknick@naver.com>
2016-12-03 03:36:01 +0900

    검색기능완료 12월 3일 ~~~~~~~~~~

--- Note_jonghun/src/com/example/note/NoteSearch.java
+++ Note_jonghun/src/com/example/note/NoteSearch.java
@@ -16,11 +16,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class NoteSearch extends ListActivity {
 	EditText searchEt;
 	Button searchBt;
-	ListView list; 
+	ListView list;
 	private static final int ACTIVITY_CREATE=0;
     private static final int ACTIVITY_EDIT=1;
 	private NotesDbAdapter mDbHelper;
@@ -34,12 +35,13 @@ public class NoteSearch extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.note_search);
 		mDbHelper = new NotesDbAdapter(this);
-		list=(ListView)findViewById(R.id.lists);
+	//	list=(ListView)findViewById(R.id.lists);
 		mDbHelper.open();
 		database = mDbHelper.getReadable();
+		//list=(ListView)findViewById(R.id.)
 		searchEt=(EditText)findViewById(R.id.searchInput);
 		searchBt=(Button)findViewById(R.id.searchBtn);
-		//registerForContextMenu(getListView());
+		registerForContextMenu(getListView());
 		Button addnote=(Button)findViewById(R.id.addnotebutton_two);
 		addnote.setOnClickListener(new View.OnClickListener() {		
 			@Override
@@ -57,28 +59,37 @@ public class NoteSearch extends ListActivity {
 	
 	
 	}
-	private void fillData(String data) {
+	private void fillData(String hash) {
         // Get all of the notes from the database and create the item list
-        Cursor cursor = mDbHelper.fetchAllNotes();
-        database=mDbHelper.getReadable();
-        database.beginTransaction();
-        //database.rawQuery("select * from notes where hash = '"+hash+"' OR hashh='"+hash+"' OR hashhh='"+hash+"';", null);
-       cursor=database.rawQuery("select * from notes where hash = '"+data+"';", null);
+       try{
+		Cursor cursor = mDbHelper.fetchAllNotes();
+    //    database=mDbHelper.getReadable();
+      //  database.beginTransaction();
+      cursor= database.rawQuery("select * from notes where hash = '"+hash+"' OR hashh = '"+hash+"' OR hashhh= '"+hash+"';", null);
+      // cursor=database.rawQuery("select * from notes;", null);
         startManagingCursor(cursor);
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),R.layout.notes_row,cursor,new String[]{NotesDbAdapter.KEY_TITLE ,NotesDbAdapter.KEY_DATE},new int[]{R.id.no_title,R.id.date_row});
-        list.setAdapter(adapter);
-        database.setTransactionSuccessful();
-        database.close();
-        
+      setListAdapter(adapter);
+      //  database.setTransactionSuccessful();
+        }catch(Exception e){
+        	 Toast.makeText(getApplicationContext(), e.getMessage(),1).show(); 
+        }finally{
+     //  database.endTransaction();
+        }
         // Now create an array adapter and set it to display using our row
-       
+   //    database.close();
       
     }
 	private void createNote(){
 		Intent i = new Intent(this,NoteEdit.class);
 		startActivityForResult(i,ACTIVITY_CREATE);
 	}
-
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent i = new Intent(this, NoteEdit.class);
+        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
 
 public boolean onCreateOptionsMenu(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
